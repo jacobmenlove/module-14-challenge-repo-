63045f48939357ce2e8be6da01f95c90a1dd7ef9
Jacob Menlove <jacobmenlove1@gmail.com>
2024-11-06 07:42:20 -0500

    updates with tutor 3

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,45 +1,23 @@
-// const forceDatabaseRefresh = false;
-
-// import dotenv from 'dotenv';
-// dotenv.config();
-
-// import express from 'express';
-// import routes from './routes/index.js';
-// import { sequelize } from './models/index.js';
-
-// const app = express();
-// const PORT = process.env.PORT || 3001;
-
-// // Serves static files in the entire client's dist folder
-// app.use(express.static('../client/dist'));
-
-// app.use(express.json());
-// app.use(routes);
-
-// sequelize.sync({force: forceDatabaseRefresh}).then(() => {
-//   app.listen(PORT, () => {
-//     console.log(`Server is listening on port ${PORT}`);
-//   });
-// });
+const forceDatabaseRefresh = false;
 
 import dotenv from 'dotenv';
 dotenv.config();
 
-import { Sequelize } from 'sequelize';
+import express from 'express';
+import routes from './routes/index.js';
+import { sequelize } from './models/index.js';
 
-const sequelize = process.env.DB_URL
-  ? new Sequelize(process.env.DB_URL)
-  : new Sequelize(
-      process.env.DB_NAME || '',
-      process.env.DB_USER || '',
-      process.env.DB_PASSWORD,
-      {
-        host: 'localhost',
-        dialect: 'postgres',
-        dialectOptions: {
-          decimalNumbers: true,
-        },
-      }
-    );
+const app = express();
+const PORT = process.env.PORT || 3001;
 
-export default sequelize;
+app.use(express.static('../client/dist'));
+
+app.use(express.json());
+app.use(routes);
+
+sequelize.sync({force: forceDatabaseRefresh}).then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+  });
+});
+
